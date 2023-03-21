f50df7c41f9ea973823453996b12cebb501c9922
Natalia <detskina@list.ru>
2023-03-22 00:20:59 +0300

    task 2

---
+++ utils/main.py
@@ -0,0 +1,54 @@
+import csv
+
+
+class Items:
+    pay_rate = 0.85
+    all = []
+
+    def __init__(self, name, price, amount):
+        self.name = name
+        self.price = price
+        self.amount = amount
+
+    def calculate_total_price(self):
+        return self.price * self.amount
+
+    def apply_discount(self):
+        self.price *= self.pay_rate
+        return self.price
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name) -> str:
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            raise Exception("Длина наименования товара превышает 10 символов.")
+
+    @classmethod
+    def instantiate_from_csv(cls) -> list | str:
+        item = []
+        with open('utils/items.csv', encoding='windows-1251') as f:
+            file_reader = csv.DictReader(f)
+            for i in file_reader:
+                name_csv = i['name']
+                price_csv = int(i['price'])
+                quantity_csv = int(i['quantity'])
+                item.append(cls(name_csv, price_csv, quantity_csv))
+                return item
+
+
+    @staticmethod
+    def is_integer(num) -> bool:
+        return int(num) == num
+
+
+Items.instantiate_from_csv()  # создание объектов из данных файла
+print(len(Items.all))  # в файле 5 записей с данными по товарам
+
+print(Items.is_integer(5))
+print(Items.is_integer(5.0))
+print(Items.is_integer(5.5))
