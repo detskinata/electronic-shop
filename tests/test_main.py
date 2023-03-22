e8a13662ef96214d57bce8702cd525c58cd1fe9f
Natalia <detskina@list.ru>
2023-03-22 22:41:37 +0300

    task 3 add

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,11 @@
-from main import *
+from utils.main import Items
+import pytest
 
 item = Items('phone', 10000, 2)
 
 
 def test_init():
-    assert item.product == 'phone'
+    assert item.name == 'phone'
     assert item.price == 10000
     assert item.amount == 2
 
@@ -12,5 +13,21 @@ def test_init():
 def test_calculate_total_price():
     assert item.calculate_total_price() == 20000
 
+
 def test_apply_discount():
-    assert item.apply_discount() == 8000.0
+    assert item.apply_discount() == 8500.0
+
+
+def test_name():
+    name = "смартфон"
+    assert name == "смартфон"
+
+def test_repr():
+    assert item.__repr__() == 'phone, 8500.0, 2'
+
+
+def test_str():
+    assert item.__str__() == 'phone'
+
+
+
